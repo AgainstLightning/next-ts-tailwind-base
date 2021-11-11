b03122c2413c58367d57049e94242b81eb36bf56
AgainstLightning <againstlightning@gmail.com>
2021-11-11 02:56:58 -0500

    fix postcss imports

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: [
-    'tailwindcss',
-    'postcss-preset-env',
-    'autoprefixer'
+    require('tailwindcss'),
+    require('postcss-preset-env'),
+    require('autoprefixer')
   ],
 }
